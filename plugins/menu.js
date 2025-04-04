d215be73ebe1ed3132d2955914a0ef3f00e66b41
CRISS VEVO <crissvevo@icloud.com>
2025-04-05 00:42:56 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -344,7 +344,7 @@ ${menuResponse}
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
             newsletterJid: '120363398040175935@newsletter',
-            newsletterName: "JawadTechX",
+            newsletterName: "CrissTechX",
             serverMessageId: 143
           }
         }
