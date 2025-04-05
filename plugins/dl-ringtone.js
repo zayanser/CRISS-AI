40c400fd35243edca9870a40e27feaf6b4295b41
CRISS VEVO <crissvevo@icloud.com>
2025-04-06 01:54:53 +0300

    Update dl-ringtone.js

--- plugins/dl-ringtone.js
+++ plugins/dl-ringtone.js
@@ -34,8 +34,8 @@ const ringtone = async (m, Matrix) => {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363354023106228@newsletter',
-          newsletterName: "JawadTechX",
+          newsletterJid: '120363378608564635@newsletter',
+          newsletterName: "CRISS AI SUPPORT",
           serverMessageId: 143
         }
       }
