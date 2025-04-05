26a885edf1efe482527dbdd9e6d3b48958a26404
CRISS VEVO <crissvevo@icloud.com>
2025-04-06 01:56:24 +0300

    Update couple-pp.js

--- plugins/couple-pp.js
+++ plugins/couple-pp.js
@@ -25,8 +25,8 @@ const couplepp = async (m, Matrix) => {
       forwardingScore: 999,
       isForwarded: true,
       forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363354023106228@newsletter',
-        newsletterName: "JawadTechX",
+        newsletterJid: '120363378608564635@newsletter',
+        newsletterName: "CRISS AI SUPPORT",
         serverMessageId: 143
       }
     };
