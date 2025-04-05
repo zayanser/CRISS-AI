0fa19816908a62cd353cafac9bf23362b4e378de
CRISS VEVO <crissvevo@icloud.com>
2025-04-06 01:04:53 +0300

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -6,7 +6,7 @@ const stickerCommand = async (m, gss) => {
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
   const text = m.body.slice(prefix.length + cmd.length).trim();
 
-  const packname = global.packname || "KHAN-MD";
+  const packname = global.packname || "CRISS-AI";
   const author = global.author || "🖤🇵🇰";
 
   const validCommands = ['sticker', 's', 'autosticker'];
