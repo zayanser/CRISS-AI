21f885e8f852f0a350d57248c75edd7a3a573149
CRISS VEVO <crissvevo@icloud.com>
2025-04-06 09:30:28 +0300

    Update uploader.js

--- media/uploader.js
+++ media/uploader.js
@@ -78,7 +78,7 @@ export const webp2mp4File = async (path) => {
       const result = 'https:' + $2('div#output > p.outfile > video > source').attr('src');
       resolve({
         status: true,
-        message: "Created By KHAN-MD",
+        message: "Created By CRISS-AI",
         result: result
       });
     } catch (err) {
